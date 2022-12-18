b0fa9de4245d4d9a04f1fef0df96ac8471705fe2
Alberto Asuero <albertoasuero@gmail.com>
2022-12-18 14:22:09 +0100

    Add test

---
+++ test/test-ci.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+mkdir -p test
+
+python bigquery_to_parquet.py \
+    --input-query "SELECT * FROM carto-do-public-data.carto.geography_usa_state_2019" \
+    --output test/geography_usa_state_2019
+
+python parquet_to_bigquery.py \
+    --input test/geography_usa_state_2019 \
+    --output "cartodb-gcp-backend-data-team.geoparquet_ci.geography_usa_state_2019"
+
+  
