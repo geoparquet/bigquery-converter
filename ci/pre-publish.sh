dfa6c7e19a5f43ec082a5567f1ba239b76823f40
Alberto Asuero <albertoasuero@gmail.com>
2022-12-18 18:02:09 +0100

    Validate schema on tests

--- ci/pre-publish.sh
+++ ci/pre-publish.sh
@@ -5,10 +5,11 @@ mkdir -p public
 TABLES=("carto-do-public-data.carto.geography_usa_state_2019" \
       "carto-do-public-data.carto.geography_usa_county_2019" \
       "carto-do-public-data.carto.geography_usa_censustract_2019" \
-      "carto-do-public-data.carto.geography_usa_zcta5_2019" \ 
-      "carto-do-public-data.carto.geography_usa_blockgroup_2019")
+      "carto-do-public-data.carto.geography_usa_zcta5_2019") 
+      # "carto-do-public-data.carto.geography_usa_blockgroup_2019")
 
 for table in ${TABLES[@]}; do
+  echo "Preparing table: $table"
   python bigquery_to_parquet.py \
      --input-query "SELECT * FROM $table" \
      --mode FILE \
--- ci/test.sh
+++ ci/test.sh
@@ -4,7 +4,14 @@ mkdir -p test
 
 python bigquery_to_parquet.py \
     --input-query "SELECT * FROM carto-do-public-data.carto.geography_usa_state_2019" \
-    --output test/geography_usa_state_2019
+    --mode FILE \
+    --output test/geography_usa_state_2019.geoparquet
+
+geoparquet_validator test/geography_usa_state_2019.geoparquet
+
+python bigquery_to_parquet.py \
+    --input-query "SELECT * FROM carto-do-public-data.carto.geography_usa_state_2019" \
+    --output test/geography_usa_state_2019.geoparquet
 
 python parquet_to_bigquery.py \
     --input test/geography_usa_state_2019 \
