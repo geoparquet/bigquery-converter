69bcabd71415524f73e0a55277d5657ef9ceec1b
Alberto Asuero <albertoasuero@gmail.com>
2022-12-18 15:03:42 +0100

    Publish CI

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,7 +2,11 @@
 
 mkdir -p public
 
-python bigquery_to_parquet.py \
-    --input-query "SELECT * FROM carto-do-public-data.carto.geography_usa_state_2019" \
-    --mode FILE
-    --output public/geography_usa_state_2019.parquet
+TABLES=("carto-do-public-data.carto.geography_usa_state_2019" "carto-do-public-data.carto.geography_usa_county_2019")
+
+for table in ${TABLES[@]}; do
+  python bigquery_to_parquet.py \
+     --input-query "SELECT * FROM $table" \
+     --mode FILE \
+     --output "public/$table.parquet"
+done
