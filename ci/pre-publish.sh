eb0db4ba84f95ccc47a338a3abca7a501bdb2360
Alberto Asuero <albertoasuero@gmail.com>
2022-12-18 17:12:52 +0100

    Test small files

--- ci/pre-publish.sh
+++ ci/pre-publish.sh
@@ -2,11 +2,13 @@
 
 mkdir -p public
 
-TABLES=("carto-do-public-data.carto.geography_usa_state_2019" \
-      "carto-do-public-data.carto.geography_usa_county_2019" \
-      "carto-do-public-data.carto.geography_usa_censustract_2019" \
-      "carto-do-public-data.carto.geography_usa_zcta5_2019" \ 
-      "carto-do-public-data.carto.geography_usa_blockgroup_2019")
+# TABLES=("carto-do-public-data.carto.geography_usa_state_2019" \
+#       "carto-do-public-data.carto.geography_usa_county_2019" \
+#       "carto-do-public-data.carto.geography_usa_censustract_2019" \
+#       "carto-do-public-data.carto.geography_usa_zcta5_2019" \ 
+#       "carto-do-public-data.carto.geography_usa_blockgroup_2019")
+
+TABLES=("carto-do-public-data.carto.geography_usa_state_2019")
 
 for table in ${TABLES[@]}; do
   python bigquery_to_parquet.py \
